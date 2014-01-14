f11c4cf5ac85b439903ad7999137aaa1797b04a4
Miguel Martin <miguel.martin7.5@hotmail.com>
2014-01-14 19:18:14 +1030

    Added CMakeLists.txt Added clean.sh and install.sh scripts

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd build
+
+# Debug Builds
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON
+sudo make install
+
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF
+sudo make install
+
+# Release Builds
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
+sudo make install
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
+sudo make install
